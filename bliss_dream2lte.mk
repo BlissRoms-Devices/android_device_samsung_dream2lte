5ee6ec9297a458984f0308d75503f8d48ee27adf
Eric Park <ideamaneric@gmail.com>
2020-06-18 06:52:21 -0400

    Blissify

--- bliss_dream2lte.mk
+++ bliss_dream2lte.mk
@@ -23,11 +23,11 @@ $(call inherit-product, device/samsung/dream2lte/device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := dream2lte
-PRODUCT_NAME := lineage_dream2lte
+PRODUCT_NAME := bliss_dream2lte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G955F
 PRODUCT_MANUFACTURER := samsung
